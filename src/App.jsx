ec769984d8292b3858d3b141e50150141957c3b9
alejandro <alejo.arcila5@gmail.com>
2026-07-21 11:51:25 -0500

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
 import './App.css';
+import Tarjeta from './components/Tarjeta';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App(){
-  const items =['React', 'Node', 'JavaScript'];
-
   return(
-    <section> 
-      <div>Hola Mundo</div>
-      <ul>
-        {
-          items.map((item, index)=>(
-            <li key={index}>{item}</li>
-          ))
-        }
-      </ul>
-    </section>
-  )
+    <div>
+      <header/>
+      <main>
+        <Tarjeta />
+        <Tarjeta />
+        <Tarjeta />
+      </main>
+      <Footer/>
+      <h1>Hola Mundo</h1>
+      <Tarjeta />
+      <Tarjeta />
+      <Tarjeta />
+    </div>
+  );
 };
-
-
 
 export default App;
