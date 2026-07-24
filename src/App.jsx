b0fa4672e23a6eaa002279228a66d9fcd0213336
Arcilkk <arcilaballenalejandro@gmail.com>
2026-07-24 11:57:11 -0500

    cambios en clase

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import './App.css';
 import Tarjeta from './components/Tarjeta';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+
 
 
 function App(){
   return(
     <div>
-      <header/>
-      <main>
-        <Tarjeta />
-        <Tarjeta />
-        <Tarjeta />
-      </main>
-      <Footer/>
-      <h1>Hola Mundo</h1>
-      <Tarjeta />
-      <Tarjeta />
-      <Tarjeta />
+      <Navbar />
+      <Hero />
     </div>
   );
 };
